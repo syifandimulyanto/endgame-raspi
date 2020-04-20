76c78d726eefb8549d7fd574365ebc61822bd77a
Syifandi Mulyanto <fanfandi17@gmail.com>
2020-04-20 12:18:03 +0700

    .gitignore is now working

--- recognizer.py
+++ recognizer.py
@@ -36,14 +36,13 @@ while True:
     for(x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
         id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-
         # Check if confidence is less them 100 ==> "0" is perfect match 
-        if (confidence < 100):
-            confidence = "  {0}%".format(round(100 - confidence))
-        else:
+        if (round(100 - confidence) < 60):
             id = "unknown"
-            confidence = "  {0}%".format(round(100 - confidence))
+
+        confidence = "  {0}%".format(round(100 - confidence))
         
+        # confidence = "  {0}%".format(round(100 - confidence))
         cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
         cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
     
